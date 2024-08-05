8d9cf16018aae014b2144c0521768cb2eded0d55
priyalpatil98 <priyalpatil98@gmail.com>
2024-08-05 17:24:48 +0000

    Updated UI

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 # File to provide url/API of app using ngix/gunicorn server (Flask framework)
 
 from flask import Flask, request, jsonify
-import model  # model.py
+#import model  # model.py
 
 app = Flask(__name__)
 
-@app.route('/predict', methods=['POST'])
-def predict(title, num, sim_type):
-    data = request.get_json()
-    response = model.recommendation(data, num = num, sim_type = sim_type)  # Calling Recommendation function in model.py
-    return jsonify(response)
+health_status = True
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+# @app.route('/predict', methods=['POST'])
+# def predict(title, num, sim_type):
+#     data = request.get_json()
+#     response = model.recommendation(data, num = num, sim_type = sim_type)  # Calling Recommendation function in model.py
+#     return jsonify(response)
     
 @app.route('/test', methods=['GET', 'POST'])
 def test_connection():
@@ -22,6 +28,17 @@ def test_connection():
     
     # Acknowledge the request
     return jsonify({"status": "success", "message": "Request received successfully."}), 200
+
+@app.route('/health')
+def health():
+    if health_status:
+        resp = jsonify(health="healthy")
+        resp.status_code = 200
+    else:
+        resp = jsonify(health="unhealthy")
+        resp.status_code = 500
+
+    return resp
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
